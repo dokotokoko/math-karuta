31a909b483779552b13bced07ccc877c6ef7ef2c
kurodai77 <e2.1river630@gmail.com>
2025-03-14 00:54:48 +0900

    srcの dataのformulas.tsを編集(問題追加、imageUrlはやっていない)

--- src/data/formulas.ts
+++ src/data/formulas.ts
@@ -65,5 +65,139 @@ export const formulas: Formula[] = [
     problem: '微分の公式は？',
     formula: '(x^n)′ = nx^(n-1)',
     imageUrl: 'https://images.unsplash.com/photo-1648737962847-1a2403bcb19e?auto=format&fit=crop&q=80&w=400'
+  },
+  {
+    id: '11',
+    problem: '正弦定理の公式は？',
+    formula: 'a/sinA=b/sinB=c/sinC=2R	',
+    imageUrl: ''
+  },
+  {
+    id: '12',
+    problem: '余弦定理の公式は？',
+    formula: 'c^2=a^2+b^2-2abcosC	',
+    imageUrl: ''
+  },
+  {
+    id: '13',
+    problem: '三角形の面積は？',
+    formula: 'S=bcsinA/2',
+    imageUrl: ''
+  },
+  {
+    id: '14',
+    problem: '三角形の面積(ヘロンの公式)は？',
+    formula: 'S=√s(s-a)(s-b)(s-c)',
+    imageUrl: ''
+  },
+  {
+    id: '15',
+    problem: '三角形の内接円と面積の公式は？',
+    formula: 'S=r(a+b+c)/2',
+    imageUrl: ''
+  },
+  {
+    id: '16',
+    problem: '順列は？',
+    formula: 'nPr=n!/(n-r)!',
+    imageUrl: ''
+  },
+  {
+    id: '17',
+    problem: '円順列は？',
+    formula: '(n-1)!',
+    imageUrl: ''
+  },
+  {
+    id: '18',
+    problem: 'じゅず順列は？',
+    formula: '(n-1)!/2',
+    imageUrl: ''
   }
+  ,{
+    id: '19',
+    problem: '重複順列は？',
+    formula: 'n^r',
+    imageUrl: ''
+  },
+  {
+    id: '20',
+    problem: '組み合わせは？',
+    formula: 'nCr=nPr/r!',
+    imageUrl: ''
+  },
+  {
+    id: '21',
+    problem: '重複組み合わせは？',
+    formula: 'nHr=n+r−1Cr',
+    imageUrl: ''
+  },
+  {
+    id: '22',
+    problem: '反復試行の確率は？',
+    formula: 'nCr(P^r)(1−p)^(n-r)',
+    imageUrl: ''
+  },
+  {
+    id: '23',
+    problem: '条件つき確率は？',
+    formula: 'PA(B)=P(A∩B)/P(A)',
+    imageUrl: ''
+  },
+  {
+    id: '24',
+    problem: '確率の乗法定理は？',
+    formula: 'P(A∩B)=P(A)PA(B)',
+    imageUrl: ''
+  },
+  {
+    id: '25',
+    problem: '2次方程式の解と係数の関係の公式は？',
+    formula: 'α+β=-b/a , αβ=c/a',
+    imageUrl: ''
+  },  
+  {
+    id: '26',
+    problem: '点と直線の距離の公式は？',
+    formula: '|ax1+by1+c|/√a^2+b^2',
+    imageUrl: ''
+  },
+  {
+    id: '27',
+    problem: '円の方程式は？',
+    formula: '(x-a)^2+(y-b)^2=r^2',
+    imageUrl: ''
+  },
+  {
+    id: '28',
+    problem: '加法定理は？',
+    formula: 'sin(α±β)=sinαcosβ±cosαsinβ',
+    imageUrl: ''
+  },
+  {
+    id: '29',
+    problem: '三角関数の合成は？',
+    formula: 'asinθ+bcosθ=(√a^2+b^2)sin(θ+α)',
+    imageUrl: ''
+  },
+  {
+    id: '30',
+    problem: '接線の方程式は？',
+    formula: 'y-f(a)=f´(a)(x-a)',
+    imageUrl: ''
+  },
+  {
+    id: '31',
+    problem: '法線の方程式は？',
+    formula: 'y-f(a)=-1/(f´(a)) (x-a)',
+    imageUrl: ''
+  },
+  {
+    id: '32',
+    problem: '',
+    formula: '',
+    imageUrl: ''
+  },
+
+
 ];
